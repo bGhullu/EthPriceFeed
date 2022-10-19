fa9c8791b8c6ae94df442a7a56b4a30961672c43
bGhullu <ghullu.bijay@gmail.com>
2022-10-19 22:28:48 +0100

    constructor removed

--- contracts/TokenPrice.sol
+++ contracts/TokenPrice.sol
@@ -7,13 +7,13 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 contract TokenPrice {
     AggregatorV3Interface private priceFeedAddress;
 
-    constructor(address _priceFeedAddress) {
-        priceFeedAddress = AggregatorV3Interface(_priceFeedAddress);
-    }
+    // constructor(address _priceFeedAddress) {
+    //     priceFeedAddress = AggregatorV3Interface(_priceFeedAddress);
+    // }
 
-    function getPrice() public view returns (uint256) {
+    function getPrice(address _priceFeedAddress) public returns (uint256) {
         //Goerli Eth/USDAddress 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
-        //AggregatorV3Interface ethPrice = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+        priceFeedAddress = AggregatorV3Interface(_priceFeedAddress);
         (, int256 price, , , ) = priceFeedAddress.latestRoundData();
         return uint256(price * 1e10);
     }
